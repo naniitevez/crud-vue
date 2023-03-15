81a26fbd04069bd57ec48e15721915ba7bb4b641
naniitevez <nicole.tevez.97@gmail.com>
2023-03-15 16:19:46 -0300

    mapeo users desde la store

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="card" style="width: 18rem">
     <div class="card-body">
-      <h5 class="card-title">{{ name }}</h5>
-      <p class="card-text">
-        {{ adress }}
-      </p>
+      <h5 class="card-title">{{ props.name }}</h5>
+      <p class="card-text">{{ props.email }}</p>
+      <p class="card-text">Cel. {{ props.phone }}</p>
+      <hr />
+      <p>{{ props.address.street }}, {{ props.address.suite }} - {{ props.address.city }}</p>
+      <span>CP. {{ props.address.zipcode }}</span>
     </div>
   </div>
 </template>
 
 <script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  name: String,
+  email: String,
+  address: {
+    street: String,
+    suite: String,
+    city: String,
+    zipcode: String,
+  },
+  phone: String,
+});
 </script>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,29 @@
 <template>
-  <h1>HOME</h1>
-  <template v-for="user in users" :key="user.id">
-    <div>{{ user.name }}</div>
-  </template>
+  <div class="container">
+    <div class="row">
+      <CardComponent v-for="user in users" :key="user.id" 
+      :name="user.name"
+      :email="user.email"
+      :address="user.address"
+      :phone="user.phone" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import { useStore } from 'vuex'
+import CardComponent from "@/components/CardComponent.vue";
+import { onMounted, ref } from "vue";
+import { useStore } from "vuex";
 
-const store = useStore()
+const store = useStore();
 
-const users = ref([])
+const users = ref([]);
 
 onMounted(() => {
-  store.dispatch('fetchUsers').then(() => {
-    users.value = store.state.users
-  })
+  store.dispatch("fetchUsers").then(() => {
+    users.value = store.state.users;
+  });
 });
-
 </script>
 
 <style>
