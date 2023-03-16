b943ea8643bbc6dd0ccbdb81e547f689c08a3836
naniitevez <nicole.tevez.97@gmail.com>
2023-03-16 18:02:06 -0300

    agrego boton goBack

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,24 +1,59 @@
 <template>
   <nav>
-    <router-link to="/">Inicio</router-link>
+    <div class="back-button-container">
+      <button class="btn btn-light" v-if="!isHome" @click="goBack">
+        <font-awesome-icon icon="fa-solid fa-arrow-left" />Volver
+      </button>
+    </div>
+    <div class="menu-container">
+      <router-link to="/">Inicio</router-link>
+      <router-link to="/user/new">Nuevo usuario</router-link>
+    </div>
   </nav>
 </template>
 
 <script setup>
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const isHome = computed(() => route.path === "/");
+
+const goBack = () => {
+  router.go(-1);
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 nav {
   padding: 30px;
-}
+  margin-bottom: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: space-evenly;
+  box-shadow: 0px 3px 12px -3px rgba(0, 0, 0, 0.15);
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  margin-right: 10px;
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin-right: 1rem;
 
-nav a.router-link-exact-active {
-  color: #42b983;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+
+  .back-button-container {
+    button {
+      svg {
+        margin-right: 10px;
+      }
+    }
+  }
+
+  .menu-container {
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faUser, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faEnvelope, faPhone, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
-library.add(faUser, faEnvelope, faPhone);
+library.add(faUser, faEnvelope, faPhone, faArrowLeft);
 
 const app = createApp(App);
 app.use(router);
