1aba9d0469e86de450e4e524eaaa78d0df791f2c
naniitevez <nicole.tevez.97@gmail.com>
2023-03-16 10:34:03 -0300

    agrego iconos de font awesome y sass

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="card" style="width: 18rem">
     <div class="card-body">
-      <h5 class="card-title">{{ props.name }}</h5>
-      <p class="card-text">{{ props.email }}</p>
-      <p class="card-text">Cel. {{ props.phone }}</p>
+      <div class="name-container">
+        <font-awesome-icon icon="fa-solid fa-user" />
+        <h5 class="card-title">{{ props.name }}</h5>
+      </div>
+      <div class="info-container">
+        <font-awesome-icon icon="fa-solid fa-envelope" />
+        <p class="card-text">{{ props.email }}</p>
+      </div>
+      <div class="info-container">
+        <font-awesome-icon icon="fa-solid fa-phone" />
+        <p class="card-text">{{ props.phone }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -18,8 +27,24 @@ const props = defineProps({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .card {
-    margin: 1rem;
+  margin: 1rem;
+}
+
+.name-container {
+  margin-bottom: 1rem;
+  border-bottom: 1px solid black;
+}
+
+.info-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: .5rem;
+
+  svg {
+    margin-right: 0.6rem;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUser, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUser, faEnvelope, faPhone);
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
-app.mount("#app");
+app.component("font-awesome-icon", FontAwesomeIcon).mount("#app");
